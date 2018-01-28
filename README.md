48634ba81a719f616c86f1a9b699db0e66d198a1
Pavel Perestoronin <eigenein@gmail.com>
2018-01-28 23:46:44 +0100

    Add WPS. Auto-generate UUID. Update README accordingly

--- README.md
+++ README.md
@@ -1,26 +1,18 @@
 # ESP8266-based LED strip controller
 
-Intended to be controlled by https://github.com/eigenein/smart-home.
-
 ## Parts
 
 * [WeMos D1 mini compatible board](https://www.aliexpress.com/item/WeMos-D1-mini-PRO-analog-WiFi-D1-mini-PRO-integration-of-ESP8266-32Mb-flash-and-USB/32806566263.html).
 * Three [STP16NF06L](https://www.aliexpress.com/item/STP16NF06L-STP16NE06-STP16NF06-5pcs-lot/32375609225.html).
 * [5V LED strip](https://www.aliexpress.com/item/RGB-LED-Strip-Waterproof-5050-DC-5V-USB-LED-Light-Strips-Flexible-bias-Lighting-50cm-1m/32826222420.html).
 
-## Building
+## Getting started
 
-Built with [PlatformIO](http://platformio.org/).
+* Build with [PlatformIO](http://platformio.org/).
+* Hold the WPS button on your router and then click the WPS button on the device. The built-in LED will light once the device is connected to Wi-Fi and IP address is obtained.
+* Use [Android app](https://github.com/eigenein/smart-home) to discover and control the device.
 
-Before building, create `src/settings.h` and configure your Wi-Fi access point:
-
-```cpp
-static const char SSID[] = "Your Wi-Fi name";
-static const char KEY[] = "Your Wi-Fi key";
-static const char UUID[] = "00000000000000000000000000000000";
-```
-
-## mDNS
+## Device discovery
 
 Device registers itself in [mDNS](https://en.wikipedia.org/wiki/Multicast_DNS) under a default name of `ESP_XXXXXX` where `XXXXXX` is the chip number:
 
@@ -32,6 +24,10 @@ DATE: ---Tue 16 Jan 2018---
 Timestamp     A/R    Flags  if Domain               Service Type         Instance Name
 20:11:17.430  Add        2   7 local.               _smart-home._udp.    ESP_380D93
 ```
+
+## Device unique identifier
+
+The device mounts SPIFFS and looks for the file `/uuid.txt`. If the file is missing, a new random UUID is generated and written to the file.
 
 ## Communication
 
@@ -57,7 +53,7 @@ The device always responds with its current state:
 {
     "messageId": 42,
     "millis": 1110940,
-    "uuid": "00000000000000000000000000000000",
+    "uuid": "00000000-0000-0000-0000-000000000000",
     "deviceType": "RGB"
 }
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
+#include <FS.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
-
-#include "settings.h"
+#include <ESP8266TrueRandom.h>
 
 // Constants.
 // ----------------------------------------------------------------------------
@@ -13,24 +13,68 @@ const int PORT = 5555;
 const int PIN_RED = D1;
 const int PIN_GREEN = D2;
 const int PIN_BLUE = D3;
+const int PIN_WPS = D5;
 
 // Setting up.
 // ----------------------------------------------------------------------------
 
 WiFiUDP udp;
 WiFiEventHandler wifiConnectedHandler, wifiGotIpHandler, wifiDisconnected;
+String uuid;
+
+void startWPS() {
+    // WPS button interrupt handler.
+    Serial.println("Starting WPS.");
+    WiFi.beginWPSConfig();
+}
 
 void setupPins() {
+    // Built-in LED displays Wi-Fi connection status.
     pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+
     pinMode(PIN_RED, OUTPUT);
     pinMode(PIN_GREEN, OUTPUT);
     pinMode(PIN_BLUE, OUTPUT);
+
+    // WPS button.
+    pinMode(PIN_WPS, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PIN_WPS), startWPS, FALLING);
 }
 
 void setupSerial() {
     Serial.begin(9600);
     Serial.println();
     Serial.println("Serial is ready.");
+}
+
+void setupStrip() {
+    // TODO: read state from the FS.
+    analogWrite(PIN_RED, 128);
+    analogWrite(PIN_GREEN, 128);
+    analogWrite(PIN_BLUE, 128);
+}
+
+void setupFS() {
+    Serial.print("Setting up file system: ");
+    Serial.println(SPIFFS.begin() ? "ok." : "failed.");
+}
+
+void setupUUID() {
+    File uuidFile = SPIFFS.open("/uuid.txt", "r");
+    if (uuidFile) {
+        Serial.print("Reading UUID: ");
+        uuid = uuidFile.readStringUntil('\n');
+    } else {
+        Serial.print("Generating UUID: ");
+        byte uuidBuffer[16];
+        ESP8266TrueRandom.uuid(uuidBuffer);
+        uuid = ESP8266TrueRandom.uuidToString(uuidBuffer);
+        uuidFile = SPIFFS.open("/uuid.txt", "w");
+        uuidFile.println(uuid);
+    }
+    uuidFile.close();
+    Serial.println(uuid);
 }
 
 void setupWiFi() {
@@ -40,16 +84,21 @@ void setupWiFi() {
     wifiGotIpHandler = WiFi.onStationModeGotIP([](const WiFiEventStationModeGotIP& event) {
         Serial.print("Got IP: ");
         Serial.println(WiFi.localIP());
+        digitalWrite(LED_BUILTIN, LOW);
     });
     wifiDisconnected = WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected& event) {
         Serial.println("Disconnected from Wi-Fi.");
+        digitalWrite(LED_BUILTIN, HIGH);
     });
-    WiFi.begin(SSID, KEY);
+
+    Serial.println("Connecting to Wi-Fi.");
+    WiFi.begin();
 }
 
 void setupMDNS() {
     Serial.print("Setting up mDNS. Hostname: ");
     Serial.println(WiFi.hostname());
+
     if (MDNS.begin(WiFi.hostname().c_str())) {
         Serial.println("Registering mDNS service.");
         MDNS.addService("smart-home", "udp", PORT);
@@ -59,16 +108,16 @@ void setupMDNS() {
 }
 
 void setupUDP() {
-    if (udp.begin(PORT)) {
-        Serial.println("UDP started.");
-    } else {
-        Serial.println("UDP start error.");
-    }
+    Serial.print("Starting UDP: ");
+    Serial.println(udp.begin(PORT) ? "ok." : "failed.");
 }
 
 void setup() {
     setupPins();
     setupSerial();
+    setupStrip();
+    setupFS();
+    setupUUID();
     setupWiFi();
     setupMDNS();
     setupUDP();
@@ -90,7 +139,7 @@ void sendResponse(JsonObject& message) {
     response["messageId"] = message.get<int>("messageId");
     response["millis"] = millis();
     response["deviceType"] = "RGB";
-    response["uuid"] = UUID;
+    response["uuid"] = uuid;
     sendPacket(response, udp.remoteIP(), udp.remotePort());
 }
 
@@ -122,13 +171,14 @@ void handlePacket() {
         return;
     }
 
-    if (type == F("ping")) {
+    if (type == "ping") {
         handlePing(message);
-    } else if (type == F("setColor")) {
+    } else if (type == "setColor") {
         handleSetColor(message);
     }
 }
 
 void loop() {
     handlePacket();
+    yield();
 }
